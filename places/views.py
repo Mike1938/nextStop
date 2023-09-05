80ecc05d7e174a8224443d6d351874369e8a4527
Mike1938 <mmaldonadosan1938@gmail.com>
2023-09-04 17:15:35 -0700

    added query to show all places listed on allplaces.html

--- places/views.py
+++ places/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 
 from .models import Places
+from links.models import Links
 # Create your views here.
 
 def all_places_view(request):
-
-    return render(request, 'allplaces.html', {})
+    obj = Places.objects.all()
+    data = {
+        "object": obj
+    }
+    return render(request, 'allplaces.html', data)
